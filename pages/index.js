a60acdcca9fee1fc1cb3fb3d3a6df325d07e7b7c
Spiielberg <spiielberg@gmail.com>
2021-01-18 15:17:15 -0300

    teste de link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 function Home() {
   return (
     <>
-      <h1>Página Inicial</h1>
+      <h1>Home</h1>
+      <Link href='/sobre'>
+        <a>Acessar página Sobre</a>
+      </Link>
     </>
   );
 }
