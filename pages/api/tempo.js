55fd9e50f71170522f68da32e0b266ec17c80d7c
Spiielberg <spiielberg@gmail.com>
2021-02-15 13:05:56 -0300

    adicionando produtos

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,8 +1,18 @@
-function tempo(req, res) {
+async function tempo(req, res) {
+  const apiSecret = process.env.API_SECRET;
   const dynamicDate = new Date();
+
+  const subscribersResponse = await fetch(
+    `https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`
+  );
+  const subscribersResponseJson = await subscribersResponse.json();
+  const inscritos = subscribersResponseJson.total_subscribers;
+
+  console.log(inscritos);
 
   res.json({
     date: dynamicDate.toUTCString(),
+    inscritos: inscritos,
   });
 }
 
